69dd5dbb820d9aabef7f3ff0e7ff42b96242a71e
lsj90954511 <sojin49@naver.com>
2025-01-24 11:23:31 +0900

    feat: Add method access logging using AOP(#2)

--- MetanetProject/src/main/java/com/example/myapp/config/WebMvcConfig.java
+++ MetanetProject/src/main/java/com/example/myapp/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import com.example.myapp.common.filter.LoginInterceptor;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class WebMvcConfig implements WebMvcConfigurer {
 	
 	@Bean
