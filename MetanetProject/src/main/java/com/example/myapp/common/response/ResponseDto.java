36ef61c450bd3c3216af2d39d93165cb22cf37d2
chaeeun-Han <chaeeun.h911@gmail.com>
2025-02-02 20:36:01 +0900

    feat: Add common response code and message (#12)

---
+++ MetanetProject/src/main/java/com/example/myapp/common/response/ResponseDto.java
@@ -0,0 +1,100 @@
+package com.example.myapp.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String code;
+    private String message;
+
+    public ResponseDto() {
+        this.code = ResponseCode.SUCCESS;
+        this.message = ResponseMessage.SUCCESS;
+    }
+
+    // HTTP Status 400
+    public static ResponseEntity<ResponseDto> notExistUser() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistEmail() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.NOT_EXISTED_EMAIL, ResponseMessage.NOT_EXISTED_EMAIL);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> duplicateEmail() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DUPLICATE_EMAIL, ResponseMessage.DUPLICATE_EMAIL);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> duplicatedId() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DUPLICATE_ID, ResponseMessage.DUPLICATE_ID);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> validationFail() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.VALIDATION_FAILED, ResponseMessage.VALIDATION_FAILED);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    // HTTP Status 401
+    public static ResponseEntity<ResponseDto> signInFail() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.SIGN_IN_FAILED, ResponseMessage.SIGN_IN_FAILED);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> certificateFail() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.CERTIFICATE_FAIL, ResponseMessage.CERTIFICATE_FAIL);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> noAuthentication() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.AUTHORIZATION_FAIL, ResponseMessage.AUTHORIZATION_FAIL);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> invalidGrant() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.INVALID_GRANT, ResponseMessage.INVALID_GRANT);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> expiredToken() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.EXPIRED_TOKEN, ResponseMessage.EXPIRED_TOKEN);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    // HTTP Status 403
+    public static ResponseEntity<ResponseDto> noPermission() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.NO_PERMISSION, ResponseMessage.NO_PERMISSION);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(responseBody);
+    }
+
+
+    // HTTP Status 500
+    public static ResponseEntity<ResponseDto> mailSendFail() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.MAIL_FAIL, ResponseMessage.MAIL_FAIL);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> redisError() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.REDIS_ERROR, ResponseMessage.REDIS_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+
+    public static ResponseEntity<ResponseDto> serverError() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.SERVER_ERROR, ResponseMessage.SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+}
---
+++ MetanetProject/src/main/java/com/example/myapp/common/response/ResponseMessage.java
@@ -0,0 +1,29 @@
+package com.example.myapp.common.response;
+
+public interface ResponseMessage {
+    // HTTP Status 200
+    String SUCCESS = "success";
+
+    // HTTP Status 400
+    String NOT_EXISTED_USER = "This user does not exist.";
+    String NOT_EXISTED_EMAIL = "This email does not exist.";
+    String DUPLICATE_EMAIL = "duplicate email";
+    String DUPLICATE_ID = "duplicate id";
+    String VALIDATION_FAILED = "validation failed";
+
+    // HTTP Status 401
+    String SIGN_IN_FAILED = "Login information mismatch.";
+    String CERTIFICATE_FAIL = "certification failed";
+    String AUTHORIZATION_FAIL = "Authorization Failed.";
+    String INVALID_GRANT = "The provided access grant is invalid, expired, or revoked";
+    String EXPIRED_TOKEN = "This token is expired";
+
+    // HTTP Status 403
+    String NO_PERMISSION = "Do not have permission.";
+
+    // HTTP Status 500
+    String MAIL_FAIL = "mail send failed";
+    String REDIS_ERROR = "An error occurred while processing Redis.";
+    String DATABASE_ERROR = "database error";
+    String SERVER_ERROR = "server error";
+}
