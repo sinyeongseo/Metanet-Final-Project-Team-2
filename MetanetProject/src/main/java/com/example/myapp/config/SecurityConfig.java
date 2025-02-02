559fc88432cf737ad9ac8a6184bb1d6314999f7b
sinyeongseo <jasmine88368@naver.com>
2025-02-02 17:59:25 +0900

    refactor: Fix Encoder BCryptPasswordEncoder (#8)

--- MetanetProject/src/main/java/com/example/myapp/config/SecurityConfig.java
+++ MetanetProject/src/main/java/com/example/myapp/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -64,6 +64,6 @@ public class SecurityConfig {
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
 }
