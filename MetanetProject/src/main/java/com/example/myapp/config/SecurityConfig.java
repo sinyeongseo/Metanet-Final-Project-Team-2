b57585abb35239c0718b7b9c815bdb6acc8cee84
udongmandu <qjawns10000@naver.com>
2025-02-05 21:40:46 +0900

    feat: Add Lecture get url auth

--- MetanetProject/src/main/java/com/example/myapp/config/SecurityConfig.java
+++ MetanetProject/src/main/java/com/example/myapp/config/SecurityConfig.java
@@ -35,7 +35,9 @@ public class SecurityConfig {
 				.requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
 				.requestMatchers("/auth/**").permitAll()
 				.requestMatchers("/email/**").permitAll()
-				.requestMatchers("/lecture/all", "lecture/{lecture_id}", "lecture/like/**").permitAll()
+				.requestMatchers("/lecture/all", "lecture/{lecture_id}", "lecture/like/**",
+						"lecture/{review_id}/reviews")
+				.permitAll()
 				// .requestMatchers("/board/test").hasAnyRole("User", "Teacher")
 				.anyRequest().authenticated() // 모든 요청은 인증이 필요
 		);
